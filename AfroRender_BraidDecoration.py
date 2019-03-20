f0b697877685aac0e207b553a4d6e5ea1547bb2b
sofya <sneechie@stanford.edu>
2019-03-19 21:58:19 -0700

    stacked beads correct direction i think

--- AfroRender_BraidDecoration.py
+++ AfroRender_BraidDecoration.py
@@ -5,14 +5,13 @@ import numpy as np
 from mathutils import Vector
 import random
 
+#Warning: Tool may be used multiple times but may crash if user tries to undo and use again after undoing operator. If you run into that issue, re-run the script after undoing. 
 scene = bpy.context.scene
-#Number of particle systems currently on the object
 
-
-    #Three different beading patterns to choose from:
-        #1. Random Distrbution - randomly distributes beads or rings about particle system (call distribute_beads function) 
-        #2. Stacked Beads - stack beads at the bottom of each braid (call stack_beads function)
-        #3. Cornrow Rings - same thing as random distribution but orients selected ring geometry to be almost perpendicular to braid (call cornrow_rings)
+#Three different beading patterns to choose from:
+    #1. Random Distrbution - randomly distributes beads or rings about particle system (call distribute_beads function) 
+    #2. Stacked Beads - stack beads at the bottom of each braid (call stack_beads function)
+    #3. Cornrow Rings - same thing as random distribution but orients selected ring geometry to be almost perpendicular to braid (call cornrow_rings)
 bpy.types.Scene.beading_patterns = bpy.props.EnumProperty(
                                 name = "Bead Patterns",
                                 default = "1",
@@ -23,24 +22,35 @@ bpy.types.Scene.beading_patterns = bpy.props.EnumProperty(
                                     ("2", "Rings", "Overhanging Rings", 2),
                                     ]
                             )
-    #Name of the bead geometry in hiearchy of all objects in the scene 
+#Name of the bead geometry in hiearchy of all objects in the scene 
 bpy.types.Scene.bead_name = bpy.props.StringProperty(name = "Bead Object Name", description = "Insert the name of the object to be distributed along the braids", default = "Bead_1") 
    
-    #Index of Particle System to add beads to 
+#Index of Particle System to add beads to 
 bpy.types.Scene.particle_system_index = bpy.props.IntProperty(name = "Particle System Index", description = "Index of Particle System to distribute Beads on", min = 0, max = 5)
    
-    #Number of beads to distribute. If the user wants to stack beads, this will be the number of beads per stand. Otherwise, it's the total number of beads in the particle system. 
+#Number of beads to distribute. If the user wants to stack beads, this will be the number of beads per stand. Otherwise, it's the total number of beads in the particle system. 
 bpy.types.Scene.num_beads = bpy.props.IntProperty(name = "Number of Beads", description = "Input the number of beads you want distributed or stacked per braid", min = 1, max = 100)
 
-    #Randomization of Beads or use multiple bead types. 
-        #--->With this parameter, this tool will check if there are multiple types of beads. (ie, objects with Bead 1, Bead 2, etc)
-        #---> otherwise, if there is only one type of bead, will randomly scale each instance of a bead. 
+#Randomization of Beads or use multiple bead types. 
+    #--->With this parameter, this tool will check if there are multiple types of beads. (ie, objects with Bead 1, Bead 2, etc)
+    #---> otherwise, if there is only one type of bead, will randomly scale each instance of a bead. 
 bpy.types.Scene.randomize_beads = bpy.props.BoolProperty(name = "Randomization of Beads in Stack", description = "If activated, there will be more than 1 type of bead in stack", default = False)
+
+#Method of Duplication - if user has not groomed hairstyle at all, check this as instancing is more efficient than duplicating every bead. 
+bpy.types.Scene.use_groups = bpy.props.BoolProperty(name = "Use Groups", description = "If activiated, tool will use instancing of bead stacks instead of duplicating individual beads.", default = False)
 #make a group of bead objects. 
 
-def get_dir(particle):
+def get_hair_dir(particle):
     a = particle.hair_keys[0].co
     b = particle.hair_keys[1].co
+    c = - a + b
+    d = c/np.sqrt(c[0]**2 + c[1]**2 +c[2]**2 )
+    print(d)
+    return d
+
+def get_segment_dir(seg_1, seg_2):
+    a = seg_1
+    b = seg_2
     c = - a + b
     d = c/np.sqrt(c[0]**2 + c[1]**2 +c[2]**2 )
     print(d)
@@ -81,7 +91,7 @@ def bead_instancing(context, group, bead, hair, position):
     new_instance.dupli_group = group
     new_instance.location = position
 
-    v1 = get_dir(hair) 
+    v1 = get_hair_dir(hair) 
     v0 = Vector((0,0,1))
     rot = v0.rotation_difference(v1).to_euler()
     new_instance.rotation_euler = rot 
@@ -102,38 +112,77 @@ def only_render_in_display():
 def stack_beads(ps, context, num_beads, bead, randomize):
 
     bead.select = True 
-    group = bead_grouping(ps, context, num_beads, bead, randomize)
+
+    if(scene.use_groups == True):  
+        group = bead_grouping(ps, context, num_beads, bead, randomize)
     
 
-    amp = ps.settings.kink_amplitude
-    print(amp)
+        amp = ps.settings.kink_amplitude
+        print(amp)
 
-    print("stacking beads...")
-    if check_hairs(ps) != True:
-        print("returning false. ")
-        return False 
-    hairs = context.object.particle_systems[0].particles
-    print(context.object.particle_systems[0].name)
-    print(hairs[0].hair_keys[0].co.x)
-    for i, h in enumerate(hairs): 
-        print('hair number {i}:'.format(i=i))
-        num_keys = len(h.hair_keys)
-        curr_hair = i
-        if(num_keys < 3): 
-            print("Not enough keys on this strand.")
-            continue 
-        prev = h.hair_keys[0].co
-        for i, hv in enumerate(h.hair_keys):
+        print("stacking beads...")
+        if check_hairs(ps) != True:
+            print("returning false. ")
+            return False 
+        hairs = context.object.particle_systems[0].particles
+        print(context.object.particle_systems[0].name)
+        print(hairs[0].hair_keys[0].co.x)
+        for i, h in enumerate(hairs): 
+            print('hair number {i}:'.format(i=i))
+            num_keys = len(h.hair_keys)
+            curr_hair = i
+            if(num_keys < 3): 
+                print("Not enough keys on this strand.")
+                continue 
+            prev = h.hair_keys[0].co
+            for i, hv in enumerate(h.hair_keys):
 
-            start_index = num_keys - num_beads
-            print(start_index)
-            if i == abs(start_index):
-                print("add a bead to the strand at this time")
+                start_index = num_keys - num_beads
+                #print(start_index)
+                if i == abs(start_index):
+                    print("add a bead to the strand at this time")
                  
                # print('  vertex {i} coordinates: {co}'.format(i=i, co=hv.co))
-                bead_instancing(context, group, bead, h, hv.co)
-                prev= hv.co
+                    bead_instancing(context, group, bead, h, hv.co)
+                    prev= hv.co
                 
+    else: 
+        print("not using groups... find another way to get directions right")
+        hairs = context.object.particle_systems[0].particles
+        strand_length = context.object.particle_systems[0].settings.hair_length 
+        bead_length = bead.dimensions[2]
+        for i, h in enumerate(hairs): 
+            print('hair number {i}:'.format(i=i))
+            num_keys = len(h.hair_keys)
+            curr_hair = i
+            if(num_keys < 3): 
+                print("Not enough keys on this strand.")
+                continue 
+            seg_length = strand_length / num_keys
+            print(seg_length)
+            prev = h.hair_keys[0].co
+            cumulative_length = 0 
+            for i, hv in enumerate(h.hair_keys):
+
+                start_index = num_keys - num_beads
+                #print(start_index)
+                if i >= abs(start_index) and cumulative_length >= bead_length:
+                    print("add a bead to the strand at this time")
+
+                    new_bead = bead.copy()
+                    new_bead.data = bead.data.copy()
+
+                    new_bead.location = hv.co
+                    v1 = get_segment_dir(prev, hv.co)
+                    v0 = Vector((0,0,1))
+                    rot = v0.rotation_difference(v1).to_euler()
+                    new_bead.rotation_euler = rot 
+                    scene.objects.link(new_bead)
+                    prev = hv.co
+                    cumulative_length = 0
+
+                cumulative_length += seg_length 
+   
     #make sure the number of keys in each strand is at least 50. 
     #for each hair strand get the keys. 
     #for every key with index greater than num_beads_on_each_braid(user-specified) 
@@ -159,7 +208,7 @@ def distribute_beads(ps, context, num_beads, bead):
         new_bead.data = bead.data.copy()
 
         new_bead.location = new_pos
-        v1 = get_dir(random_hair)
+        v1 = get_hair_dir(random_hair)
         v0 = Vector((0,0,1))
         rot = v0.rotation_difference(v1).to_euler()
         new_bead.rotation_euler = rot 
@@ -279,6 +328,9 @@ class AfroRender_BraidDecoPanel(bpy.types.Panel):
 
         row = self.layout.row(align=True)
         row.prop(context.scene, "randomize_beads")
+
+        row = self.layout.row(align=True)
+        row.prop(context.scene, "use_groups")
 
 
 
